62f0fa2196f4a0bb4a37d5110a969be44ee2f4d2
id1179918 <52131528+id1179918@users.noreply.github.com>
2023-03-20 13:27:38 +0100

    Adding 2 firmwares for the 16u2 micro-processor, and an example usage of huidino

--- sketch_jan25a.ino
+++ sketch_jan25a.ino
@@ -1,8 +1,14 @@
 #include <Control_Surface.h>
+//#include <MIDI_Interfaces/USBMIDI_Interface.hpp>
  
-HairlessMIDI_Interface midi {9600};
+USBMIDI_Interface midi;
+USBDebugMIDI_Interface mididbg;
+BidirectionalMIDI_Pipe mpipe;
 
 CCPotentiometer potentiometer { A0, {MIDI_CC::Channel_Volume, CHANNEL_1} };
 
-void setup() { Control_Surface.begin(); }
+void setup() { 
+  midi | mpipe | mididbg;
+  Control_Surface.begin();
+}
 void loop() { Control_Surface.loop(); }
