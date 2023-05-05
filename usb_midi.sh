63e55d85acdeb11bdbf553df38ad47cd01a0bf96
ROUSTAN Thomas <thomas.roustan@miraex.com>
2023-05-05 20:57:26 +0200

    working multiplexer and single po

--- usb_midi.sh
+++ usb_midi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo dfu-programmer atmega16u2 erase
+sudo dfu-programmer atmega16u2 erase --force
 
-sudo dfu-programmer atmega16u2 flash arduino-midi.hex
+sudo dfu-programmer atmega16u2 flash arduino-midi.hex --suppress-bootloader-mem
 
 sudo dfu-programmer atmega16u2 reset
--- usb_serial.sh
+++ usb_serial.sh
@@ -2,6 +2,6 @@
 
 sudo dfu-programmer atmega16u2 erase
 
-sudo dfu-programmer atmega16u2 flash usbserial_mega_16u2.hex
+sudo dfu-programmer atmega16u2 flash usbserial_mega_16u2.hex --suppress-bootloader-mem
 
 sudo dfu-programmer atmega16u2 reset
